e0a4fa742c8c51aba909b695d80789ea7cfd72a1
AslaSakydeen <aslafathi26@gmail.com>
2026-07-18 05:02:36 +0530

    update database connection to Supabase

--- gn-complaint-backend/src/db/index.ts
+++ gn-complaint-backend/src/db/index.ts
@@ -5,9 +5,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "complaint_management_system",
-    password: "ccms123",
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
 });
