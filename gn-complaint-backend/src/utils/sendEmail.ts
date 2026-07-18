04c5329c65dcbdc6066f04059febc32f5f073cb0
AslaSakydeen <aslafathi26@gmail.com>
2026-07-19 04:26:59 +0530

    Replace SMTP with email API

--- gn-complaint-backend/src/utils/sendEmail.ts
+++ gn-complaint-backend/src/utils/sendEmail.ts
@@ -1,56 +1,37 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import dotenv from "dotenv";
-
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("❌ SMTP Connection Failed");
-    console.log(error);
-  } else {
-    console.log("✅ SMTP Server Ready");
-  }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (
   to: string,
   subject: string,
   html: string
 ) => {
+
   try {
+
     console.log("📧 Sending email to:", to);
 
-    const info = await transporter.sendMail({
-      from: `"GN Complaint Management System" <${process.env.EMAIL_USER}>`,
+    await resend.emails.send({
+      from: "GN Complaint System <onboarding@resend.dev>",
       to,
       subject,
       html,
     });
 
     console.log("✅ Email sent successfully");
-    console.log("Message ID:", info.messageId);
 
     return true;
 
-  } catch (error) {
+  } catch(error){
 
-    console.error("❌ Email Error:");
-    console.error(error);
+    console.log("❌ Email Error");
+    console.log(error);
 
-    // DON'T throw the error
     return false;
   }
+
 };
